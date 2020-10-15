ca47b5ebfc29505c8a51efd489cd2081c0003725
Linar <linarka94@gmail.com>
2020-10-15 13:43:45 +0300

    add new pages

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,32 @@
 $(window).on('load', function() {
    console.log(2);
 });
+
+$(window).on('load', function () {
+   $('.marquee').liMarquee();
+});
+
+$(document).ready(function () {
+   $('.acc__title').click(function (event) {
+      // if ($('.acc').hasClass('acc--one')) {
+      //    $('.acc__title').not($(this)).removeClass('acc__title--active');
+      //    $('.acc__content').not($(this).next()).slideUp(300);
+      // } //если надо чтобы открывался только по одному блоку
+      $(this).toggleClass('acc__title--active').next().slideToggle(300);
+   })
+});
+
+
+$(window).on('load', function() {
+   $('.news__item-text').text(function(i, text) {
+
+      if (text.length >= 140) {
+         text = text.substring(0, 140);
+         const lastIndex = text.lastIndexOf(" ");       // позиция последнего пробела
+         text = text.substring(0, lastIndex) + '...'; // обрезаем до последнего слова
+      }
+
+      $(this).text(text);
+
+   });
+});
--- src/js/plugins/slick.js
+++ src/js/plugins/slick.js
@@ -5,4 +5,32 @@ $(document).ready(function() {
     $('.slick').slick({
         variableWidth: true
     });
+
+    $('.slick-place').slick({
+        variableWidth: true
+    });
+
+    $('.slick-about').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+
+    });
+
+    $('.slick-autoplay').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+
+    });
 });
